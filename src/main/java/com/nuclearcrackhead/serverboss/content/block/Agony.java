f7fecd8ecd18d5b60de2ec288105ac785578f4a0
adenator <82839009+adenator0@users.noreply.github.com>
2024-12-22 09:53:43 -0500

    why are there so many redundant casts

--- src/main/java/com/nuclearcrackhead/serverboss/content/block/Agony.java
+++ src/main/java/com/nuclearcrackhead/serverboss/content/block/Agony.java
@@ -20,7 +20,6 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
@@ -31,15 +30,12 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-
 import java.util.List;
-
-import static net.minecraft.state.property.Properties.ORIENTATION;
 
 public class Agony extends Block {
     private static final EnumProperty<Orientation> ORIENTATION;
     public static final MapCodec<Agony> CODEC = createCodec(Agony::new);
-    protected static final VoxelShape SHAPE = Block.createCuboidShape((double)1.0F, (double)0.0F, (double)1.0F, (double)16.0F, (double)15.0F, (double)16.0F);
+    protected static final VoxelShape SHAPE = Block.createCuboidShape(1.0F, 0.0F, 1.0F, 16.0F, 15.0F, 16.0F);
 
     public MapCodec<Agony> getCodec() {
         return CODEC;
@@ -59,11 +55,9 @@ public class Agony extends Block {
     }
 
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
-        if (entity instanceof LivingEntity livingEntity) { //bypass check removed since i don't think it's supposed to not agony you if you sneak
-            livingEntity.serverDamage(entity.getDamageSources().create(ModDamageTypes.AGONY_DAMAGE, null), livingEntity.getMaxHealth() / 7);
-            if (world instanceof ServerWorld serverWorld) {
-                serverWorld.spawnParticles(new DustParticleEffect(ColorHelper.fromFloats(1, 1, 0, 0), 2), livingEntity.getPos().getX(), livingEntity.getPos().getY(), livingEntity.getPos().getZ(), 4, 0.2, 0, 0.2, 0);
-            }
+        if (entity instanceof LivingEntity livingEntity && world instanceof ServerWorld serverWorld) { //bypass check removed since i don't think it's supposed to not agony you if you sneak
+            livingEntity.damage(serverWorld, entity.getDamageSources().create(ModDamageTypes.AGONY_DAMAGE, null), livingEntity.getMaxHealth() / 7);
+            serverWorld.spawnParticles(new DustParticleEffect(ColorHelper.fromFloats(1, 1, 0, 0), 2), livingEntity.getPos().getX(), livingEntity.getPos().getY(), livingEntity.getPos().getZ(), 4, 0.2, 0, 0.2, 0);
             if (!world.isClient && (livingEntity.hurtTime == 10)) {
                 world.playSound(null, pos, ModSounds.BLOCK_AGONY_ACTIVATE_FANGSNAP, SoundCategory.BLOCKS);
                 world.playSound(null, pos, ModSounds.BLOCK_AGONY_ACTIVATE_ZOMBIESTEP, SoundCategory.BLOCKS);
@@ -75,38 +69,24 @@ public class Agony extends Block {
 
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         Direction direction = ctx.getPlayerLookDirection().getOpposite();
-        Direction var10000;
-        switch (direction) {
-            case DOWN:
-                var10000 = ctx.getHorizontalPlayerFacing().getOpposite();
-                break;
-            case UP:
-                var10000 = ctx.getHorizontalPlayerFacing();
-                break;
-            case NORTH:
-            case SOUTH:
-            case WEST:
-            case EAST:
-                var10000 = Direction.UP;
-                break;
-            default:
-                throw new MatchException((String)null, (Throwable)null);
-        }
-
-        Direction direction2 = var10000;
-        return (BlockState)((BlockState)this.getDefaultState().with(ORIENTATION, Orientation.byDirections(direction, direction2)));
+        Direction direction2 = switch (direction) {
+            case DOWN -> ctx.getHorizontalPlayerFacing().getOpposite();
+            case UP -> ctx.getHorizontalPlayerFacing();
+            case NORTH, SOUTH, WEST, EAST -> Direction.UP;
+        };
+        return (this.getDefaultState().with(ORIENTATION, Orientation.byDirections(direction, direction2)));
     }
 
     protected BlockState rotate(BlockState state, BlockRotation rotation) {
-        return (BlockState)state.with(ORIENTATION, rotation.getDirectionTransformation().mapJigsawOrientation((Orientation)state.get(ORIENTATION)));
+        return state.with(ORIENTATION, rotation.getDirectionTransformation().mapJigsawOrientation(state.get(ORIENTATION)));
     }
 
     protected BlockState mirror(BlockState state, BlockMirror mirror) {
-        return (BlockState)state.with(ORIENTATION, mirror.getDirectionTransformation().mapJigsawOrientation((Orientation)state.get(ORIENTATION)));
+        return state.with(ORIENTATION, mirror.getDirectionTransformation().mapJigsawOrientation(state.get(ORIENTATION)));
     }
 
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{ORIENTATION});
+        builder.add(ORIENTATION);
     }
 
     static {
