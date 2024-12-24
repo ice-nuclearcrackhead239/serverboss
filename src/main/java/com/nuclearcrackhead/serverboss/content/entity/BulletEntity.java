e99f9b141143e2c02ecd41c363748986f9b41c9b
finxx <finxx@finxxs-MacBook-Air.local>
2024-12-24 17:05:47 -0600

    make bullets move

--- src/main/java/com/nuclearcrackhead/serverboss/content/entity/BulletEntity.java
+++ src/main/java/com/nuclearcrackhead/serverboss/content/entity/BulletEntity.java
@@ -5,11 +5,17 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.World;
 import net.minecraft.entity.data.DataTracker;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.MathHelper;
 
+import com.nuclearcrackhead.serverboss.SVBCR;
 import com.nuclearcrackhead.serverboss.registry.ModEntities;
 
 public class BulletEntity extends ProjectileEntity {
 	public int damage = 1;
+	public float speed;
+	public float pitch;
+	public float yaw;
 
 	public BulletEntity(EntityType<? extends ProjectileEntity> entityType, World world) {
 		super(entityType, world);
@@ -22,13 +28,29 @@ public class BulletEntity extends ProjectileEntity {
 	}
 
 	public BulletEntity setDirection(float pitch, float yaw) {
-		this.setRotation(yaw, pitch);
+		this.pitch = pitch;
+		this.yaw = yaw;
+		fixVelocity();
 		return this;
 	}
 
 	public BulletEntity setSpeed(float speed) {
-		this.setVelocity(0, speed, 0);
+		this.speed = speed;
+		fixVelocity();
 		return this;
+	}
+
+	public void fixVelocity() {
+		float f = -MathHelper.sin(yaw * (float) (Math.PI / 180.0)) * MathHelper.cos(pitch * (float) (Math.PI / 180.0));
+		float g = -MathHelper.sin(pitch * (float) (Math.PI / 180.0));
+		float h = MathHelper.cos(yaw * (float) (Math.PI / 180.0)) * MathHelper.cos(pitch * (float) (Math.PI / 180.0));
+		Vec3d vel = new Vec3d(f, g, h).multiply(speed);
+		SVBCR.LOGGER.info("{}", vel);
+		setVelocity(vel);
+	}
+
+	public void tick() {
+		setPosition(getPos().add(getVelocity()));
 	}
 
 	public BulletEntity setDamage(int damage) {
