ee439d26f0d1b55e028f74f8f132a7cf69b1c05f
finxx <finxx@finxxs-MacBook-Air.local>
2024-12-25 18:59:21 -0600

    worlds greatest animations

--- src/client/java/com/nuclearcrackhead/serverboss/mixin/client/HeldItemRendererMixin.java
+++ src/client/java/com/nuclearcrackhead/serverboss/mixin/client/HeldItemRendererMixin.java
@@ -7,6 +7,9 @@ import net.minecraft.util.Arm;
 import net.minecraft.item.ItemStack;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.util.Hand;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.item.ModelTransformationMode;
+import net.minecraft.client.render.OverlayTexture;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,10 +24,16 @@ import com.nuclearcrackhead.serverboss.content.item.IGun;
 
 @Mixin(HeldItemRenderer.class)
 public class HeldItemRendererMixin {
+	@Shadow ItemRenderer itemRenderer;
+
 	@Inject(at = @At("HEAD"), method = "renderFirstPersonItem", cancellable = true)
 	private void renderGun(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
 		if (item.getItem() instanceof IGun gun) {
-			// todo render
+			matrices.push();
+			matrices.translate(0.5, -0.5, -0.5);
+			matrices.translate(0, swingProgress, 0);
+			itemRenderer.renderItem(player, item, ModelTransformationMode.FIRST_PERSON_RIGHT_HAND, false, matrices, vertexConsumers, player.getWorld(), light, OverlayTexture.DEFAULT_UV, 2);
+			matrices.pop();
 			ci.cancel();
 		}
 	}
