1ee22fd876778f1da8edeb771b6e019d26c09b4b
soumeh <soumeh@proton.me>
2025-04-05 21:20:26 +0200

    fixed sky color provider

--- src/client/java/com/nuclearcrackhead/serverboss/registry/ModColorProviders.java
+++ src/client/java/com/nuclearcrackhead/serverboss/registry/ModColorProviders.java
@@ -8,42 +8,40 @@ import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.color.world.BiomeColors;
 import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Colors;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockRenderView;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.GrassColors;
 
 public class ModColorProviders {
 
+    public static final int DEFAULT_SKY_COLOR = 7907327;
+
+    public static int getSkyColor(BlockState state, BlockRenderView view, BlockPos pos, int tintIndex) {
+        if (view == null || pos == null) return Colors.WHITE;
+        RegistryEntry<Biome> biomeEntry = view.getBiomeFabric(pos);
+        if (biomeEntry == null) return DEFAULT_SKY_COLOR;
+        return biomeEntry.value().getSkyColor();
+    }
 
     public static void init() {
         //blocks
         ColorProviderRegistry.BLOCK.register(
-                (state, view, pos, tintIndex) -> view != null && pos != null ? view.getBiomeFabric(pos).value().getSkyColor() : Colors.WHITE,
-                ModBlocks.B_DEV_SKY
-        );
-        ColorProviderRegistry.BLOCK.register(
-                (state, view, pos, tintIndex) -> view != null && pos != null ? view.getBiomeFabric(pos).value().getSkyColor() : Colors.WHITE,
+                ModColorProviders::getSkyColor,
+                ModBlocks.B_DEV_SKY,
                 ModBlocks.VOLUMETRIC_LIGHT
         );
-
         ColorProviderRegistry.BLOCK.register(
                 (state, view, pos, tintIndex) -> view != null && pos != null ? BiomeColors.getGrassColor(view, pos) : Colors.WHITE,
-                ModBlocks.WATER_SILK
+                ModBlocks.WATER_SILK,
+                Blocks.SEAGRASS,
+                Blocks.TALL_SEAGRASS
         );
         ColorProviderRegistry.BLOCK.register(
                 (state, view, pos, tintIndex) -> view != null && pos != null ? BiomeColors.getWaterColor(view, pos) : Colors.WHITE,
                 ModBlocks.PUDDLE
-        );
-
-
-        ColorProviderRegistry.BLOCK.register(
-                (state, view, pos, tintIndex) -> view != null && pos != null ? BiomeColors.getGrassColor(view, pos) : Colors.WHITE,
-                Blocks.SEAGRASS
-        );
-
-
-        ColorProviderRegistry.BLOCK.register(
-                (state, view, pos, tintIndex) -> view != null && pos != null ? BiomeColors.getGrassColor(view, pos) : Colors.WHITE,
-                Blocks.TALL_SEAGRASS
         );
 
         //items
